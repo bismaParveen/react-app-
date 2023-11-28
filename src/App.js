35afa0e1456ec66f2941b59b419025302b16208d
Bisma <bismaparveen87@gmail.com>
2023-11-28 17:36:10 +0530

    this is first project

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import Users from "./components/Users";
+
+function App(props) {
+  let userList = [
+    {
+      imageUrl: "https://www.shutterstock.com/image-photo/young-beautiful-girl-preparing-exams-260nw-622036163.jpg",
+      title: "Bisma",
+      desc: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+    },
+    {
+      imageUrl:"https://c4.wallpaperflare.com/wallpaper/292/244/1008/apple-apple-computer-boy-code-wallpaper-preview.jpg",
+      title: "Alqama",
+      desc: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+    },
+    {
+      imageUrl:"https://c4.wallpaperflare.com/wallpaper/292/244/1008/apple-apple-computer-boy-code-wallpaper-preview.jpg",
+      title: "Hanzala",
+      desc: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+    },
+  ];
+  return (
+    <>
+      <Navbar title="Bisma"/>
+      <Users userList={userList}/>
+      
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Users.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Users = (props) => {
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          {props.userList.map(({ imageUrl, title, desc }, index) => {
+            return (
+              <div className="col-md-4" key={index}>
+                <div className="card">
+                  <img src={imageUrl} className="card-img-top" alt="..." />
+                  <div className="card-body">
+                    <h5 className="card-title">{title}</h5>
+                    <p className="card-text">{desc}</p>
+                    <a href="/" className="btn btn-primary">
+                      Read more
+                    </a>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Users;
